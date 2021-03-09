cdb2c2a00405261d515e3bdc65e891d347afd8ea
Nandan Rao <nandanmarkrao@gmail.com>
2021-03-09 18:46:59 +0100

    use navigo for links and improve links on tablet

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function vlab () {
 
-  function setContent(content) {
+  function setContent(name) {
     document
       .querySelectorAll('div.main > div')
       .forEach(el => el.classList.remove('visible'))
@@ -10,26 +10,40 @@
       .forEach(el => el.classList.remove('selected'))
 
     document
-      .querySelector('nav li.' + content)
+      .querySelector('nav li.' + name)
       .classList.add('selected')
 
     document
-      .querySelector('div.main > div.' + content)
+      .querySelector('div.main > div#' + name)
       .classList.add('visible')
   }
 
+  function _add_route(routes, route, name) {
+    routes[route] = function () { setContent(name) }
+    return routes
+  }
+
   var router = new Navigo(null);
+
+  function registerHandler(name, path) {
+    document
+      .querySelector('nav li.' + name)
+      .addEventListener('click', function (e) {
+        e.preventDefault()
+        router.navigate(path)
+      })
+  }
+
+  var conf = [
+    ['documentation', 'documentation', '/documentation'],
+    ['studies', 'studies', '/studies'],
+    ['*', 'overview', '']
+  ]
+
+  conf.forEach(function (c) { registerHandler(c[1], c[2]) })
   router
-    .on({
-      'documentation': function () {
-        setContent('documentation');
-      },
-      'studies': function () {
-        setContent('studies');
-      },
-      '*': function () {
-        setContent('overview')
-      }
-    })
-    .resolve();  
+    .on(conf.reduce(function (a, c) {
+      return _add_route(a, c[0], c[1])
+    }, {}))
+    .resolve()
 })()
